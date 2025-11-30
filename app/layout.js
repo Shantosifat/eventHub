d88174940a5768edd0e10dc5aaa03faf55786307
Shantosifat1 <shanto201934024@gmail.com>
2025-12-01 01:57:44 +0600

    landing page done

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
             <ConvexClientProvider>
               {/* Haeder */}
               <Header />
-              <main className="relative min-h-screen container mx-auto pt-40 md:pt-5">
+              <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
                 {/* glow */}
                 <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
                   <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
--- convex/schema.js
+++ convex/schema.js
@@ -28,4 +28,78 @@ export default defineSchema({
     createdAt: v.number(),
     updatedAt: v.number(),
   }).index("by_token", ["tokenIdentifier"]),
+
+  // events table
+
+  events: defineTable({
+    title: v.string(),
+    description: v.string(),
+    slug: v.string(),
+    // slug is the url for the particular event
+
+    // organizer
+    organizerId: v.id("users"),
+    organizerName: v.string(),
+
+    // event details
+    category: v.string(),
+    tags: v.array(v.string()),
+
+    // date & time
+    startDate: v.number(),
+    endDate: v.number(),
+    timeZone: v.string(),
+
+    // location(optional)
+    locationType: v.union(v.literal("physical"), v.literal("online")),
+    venue: v.optional(v.string()),
+    address: v.optional(v.string()),
+    city: v.string(),
+    state: v.optional(v.string()),
+    country: v.string(),
+
+    // capacity & ticketing
+    capacity: v.number(),
+    ticketType: v.union(v.literal("free"), v.literal("paid")),
+    ticketPrice: v.optional(v.number()), // Paid at event offline
+    registrationCount: v.number(),
+
+    // customization
+    coverImage: v.optional(v.string()),
+    themeColor: v.optional(v.string()),
+
+    // Timestamps
+    createdAt: v.number(),
+    updatedAt: v.number(),
+  })
+    .index("by_organizer", ["organizerId"])
+    .index("by_category", ["category"])
+    .index("by_start_date", ["startDate"])
+    .index("by_slug", ["slug"])
+    .searchIndex("search_title", { searchField: "title" }),
+
+  // registration table
+  registrations: defineTable({
+    eventId: v.id("events"),
+    userId: v.id("users"),
+
+    // attendee info
+    attendeeName: v.string(),
+    attendeeEmail: v.string(),
+
+    // QR code for entry
+    qrCode: v.string(), // unique id for qr
+
+    // check in
+    checkedIn: v.boolean(),
+    checkedInAt: v.optional(v.number()),
+
+    // status
+    status: v.union(v.literal("confirmed"), v.literal("cancelled")),
+    registeredAt: v.number(),
+  })
+    .index("by_event", ["eventId"])
+    .index("by_user", ["userId"])
+    .index("by_event_user", ["eventId", "userId"])
+    .index("by_qr_code", ["qrCode"]),
 });
